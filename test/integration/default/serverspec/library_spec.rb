dede1399c2e13cdc090a648e271812e40465eef2
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-22 16:49:20 +0100

    Extend path for serverspec tests.

--- test/integration/default/serverspec/library_spec.rb
+++ test/integration/default/serverspec/library_spec.rb
@@ -3,6 +3,11 @@ require 'serverspec'
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
+# TODO: DRY
+RSpec.configure do |c|
+  c.path = '/sbin:/usr/sbin'
+end
+
 describe 'zeromq library' do
   describe command('ldconfig -p | grep -q /usr/local/lib/libzmq') do
     it { should return_exit_status 0 }
