823b8e438d662fed7829e3047729f36bab19b666
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-22 15:05:15 +0100

    Add test.

---
+++ test/integration/default/serverspec/library_spec.rb
@@ -0,0 +1,10 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+describe 'zeromq library' do
+  describe command('ldconfig -p | grep -q libzmq') do
+    it { should return_exit_status 0 }
+  end
+end
