58e8ac1f70fe8de65c96d7e6efffa849356829f8
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-22 15:19:44 +0100

    Add custom_dir tests.

---
+++ test/integration/custom_dir/serverspec/library_spec.rb
@@ -0,0 +1,14 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+describe 'zeromq library' do
+  describe command('ldconfig -p | grep -q libzmq') do
+    it { should return_exit_status 0 }
+  end
+end
+
+describe file('/etc/ld.conf.so.d/zeromq.conf') do
+  its(:content) { should match %r|/opt/zeromq/lib| }
+end
