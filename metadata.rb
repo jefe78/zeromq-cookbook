d456d3fd231f408d8c6e0ee8be9ce76dfda4b57e
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-22 11:33:03 +0100

    Update license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'zeromq'
 maintainer       'Johannes Plunien'
 maintainer_email 'plu@pqpq.de'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures zeromq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
