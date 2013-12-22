00daabf44bea88fa28f649e3fe5403ddbd68f41a
Johannes Plunien <johannes.plunien@metaquark.de>
2013-12-22 15:24:06 +0100

    Let test be more specific.

--- test/integration/custom_dir/serverspec/library_spec.rb
+++ test/integration/custom_dir/serverspec/library_spec.rb
@@ -4,7 +4,7 @@ include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
 
 describe 'zeromq library' do
-  describe command('ldconfig -p | grep -q libzmq') do
+  describe command('ldconfig -p | grep -q /opt/zeromq/lib/libzmq') do
     it { should return_exit_status 0 }
   end
 end
