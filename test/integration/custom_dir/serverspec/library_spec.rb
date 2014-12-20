74c58b96ce6f8a8897a93bacefb482434c81aaca
Johannes Plunien <plu@pqpq.de>
2014-12-20 10:44:18 +0100

    Verify shared object is there

--- test/integration/custom_dir/serverspec/library_spec.rb
+++ test/integration/custom_dir/serverspec/library_spec.rb
@@ -2,10 +2,12 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe 'zeromq library' do
-  describe command('ldconfig -p | grep -q /opt/zeromq/lib/libzmq') do
-    its(:exit_status) { should eq 0 }
-  end
+describe command('ldconfig -p | grep -q /opt/zeromq/lib/libzmq') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/opt/lib/libzmq.so') do
+  it { should be_file }
 end
 
 describe file('/etc/ld.so.conf.d/zeromq.conf') do
--- test/integration/source/serverspec/library_spec.rb
+++ test/integration/source/serverspec/library_spec.rb
@@ -2,8 +2,10 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe 'zeromq library' do
-  describe command('ldconfig -p | grep -q /usr/local/lib/libzmq') do
-    its(:exit_status) { should eq 0 }
-  end
+describe command('ldconfig -p | grep -q /usr/local/lib/libzmq') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/usr/local/lib/libzmq.so') do
+  it { should be_file }
 end
