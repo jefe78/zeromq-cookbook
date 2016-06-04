e41859c0e7a85afba960afdf4744f49d8319e83d
Johannes Plunien <plu@pqpq.de>
2016-06-04 08:44:37 +0400

    Install libsodium

--- recipes/install_from_source.rb
+++ recipes/install_from_source.rb
@@ -18,6 +18,10 @@
 #
 
 include_recipe 'build-essential'
+include_recipe 'git::default'
+
+package 'libtool'
+package 'pkg-config'
 
 case node['platform_family']
 when 'rhel', 'fedora'
@@ -31,6 +35,38 @@ when 'debian', 'ubuntu'
     package 'uuid-dev'
   end
 end
+
+# libsodium
+
+libsodium_tar = "libsodium-#{node['zeromq']['libsodium_version']}.tar.gz"
+libsodium_tar_path = "/usr/local/src/#{libsodium_tar}"
+libsodium_src_url = node['zeromq']['libsodium_src_url']
+libsodium_src_dir = "/usr/local/src/libsodium-#{node['zeromq']['libsodium_version']}"
+
+remote_file libsodium_tar_path do
+  source libsodium_src_url
+  checksum node['zeromq']['libsodium_sha256_sum']
+  mode 0644
+  action :create_if_missing
+end
+
+directory libsodium_src_dir do
+  action :create
+end
+
+execute "tar --no-same-owner -zxf #{libsodium_tar} -C #{libsodium_src_dir} --strip-components 1" do
+  cwd "/usr/local/src"
+  creates File.join(libsodium_src_dir, 'configure')
+end
+
+execute 'libsodium compile and install' do
+  environment({'PATH' => '/usr/local/bin:/usr/bin:/bin'})
+  command "./configure --prefix=#{node['zeromq']['dir']} && make && make install"
+  cwd libsodium_src_dir
+  creates File.join(node['zeromq']['dir'], 'lib', 'libsodium.so')
+end
+
+# 0mq
 
 zeromq_src_dir = "/usr/local/src/zeromq-#{node['zeromq']['version']}"
 
